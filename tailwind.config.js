a296ced534fa1b59bc11e48fe3082b04d5980e8b
etmjr03 <etmjr03@gmail.com>
2023-10-02 00:22:02 -0300

    Adicionando tailwindcss e alpine.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio")
+  ],
+}
+
